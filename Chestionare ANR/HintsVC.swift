b3056ab5d77340f6c6442eb459a5ac300585bbb8
Marius Tanasoiu <marius.tanasoiu@yahoo.com>
2021-02-22 00:55:20 +0200

    lljk

--- Chestionare ANR/HintsVC.swift
+++ Chestionare ANR/HintsVC.swift
@@ -19,11 +19,25 @@ class HintsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad();
         
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
         containerView.layer.cornerRadius = 10;
         scrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo:label.bottomAnchor).isActive = true;
         
         var text = String();
         let hintsMap = XMLUtil.instance.hints;
+        
+        for (index, element) in hints.enumerated() {
+            text.append(hintsMap[element]!.content.replacingOccurrences(of: "[", with: "<").replacingOccurrences(of: "]", with: ">"));
+            if(index != hints.count - 1) {
+                text.append("\n\n");
+            }
+        }
+        
 //        for hintID in hints {
 //            text.append(hintsMap[hintID]!.content.replacingOccurrences(of: "[", with: "<").replacingOccurrences(of: "]", with: ">"));
 //            text.append("\n\n");
@@ -31,12 +45,20 @@ class HintsVC: UIViewController {
         
         for hintID in 1...10 {
             text.append(hintsMap[hintID]!.content.replacingOccurrences(of: "[", with: "<").replacingOccurrences(of: "]", with: ">"));
-            text.append("<br><br>");
+            
+            if(hintID != 10) {
+                text.append("<br><br>");
+            }
         }
         
-        label.attributedText = stringFromHtml(string: text);
+//        label.attributedText = stringFromHtml(string: text);
         
         label.text = text;
+        
+        
+        label.layoutIfNeeded();
+        scrollView.layoutIfNeeded();
+        containerView.layoutIfNeeded();
     }
     
     private func stringFromHtml(string: String) -> NSAttributedString? {
